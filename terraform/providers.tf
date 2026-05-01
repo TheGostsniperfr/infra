6cd0737f699b9073016f94ee8b4daec95924fdf9
TheGostsniperfr <brianperret.pro@gmail.com>
2026-05-01 15:18:17 +0200

    fix(vault): fix certificats

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,13 @@
 # Doc: https://registry.terraform.io/providers/hashicorp/vault/5.6.0/docs
 
 terraform {
+  backend "s3" {
+    bucket         = "arffornia-infra-terraform-state"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-west-3"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
   required_providers {
     vault = {
       source  = "hashicorp/vault"
@@ -20,8 +27,9 @@ terraform {
 }
 
 provider "keycloak" {
-  client_id                = "admin-cli"
-  url                      = var.keycloak_url
+  client_id = "admin-cli"
+  url       = var.keycloak_url
+  # url                      = "http://localhost:8080"
   username                 = var.keycloak_admin_user_var
   password                 = var.keycloak_admin_password_var
   tls_insecure_skip_verify = false # Set to true if using self-signed certs
@@ -29,10 +37,12 @@ provider "keycloak" {
 
 provider "vault" {
   address = var.vault_url
+  # address = "http://localhost:8200"
   # token   = "" # Set in environment variable VAULT_TOKEN or ~/.vault-token file (if both are unset)
   # ca_cert_file = "" # Path to CA cert file if using self-signed certs
 }
 
 provider "cloudflare" {
   # Configuration options
+  api_token = var.cloudflare_api_token_secret_var
 }
